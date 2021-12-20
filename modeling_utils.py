a7280811e12d19d25115471921a38a3e8c36d192
vipermu <vipermu97@gmail.com>
2021-12-20 15:40:47 +0100

    fix: indent

--- modeling_utils.py
+++ modeling_utils.py
@@ -4,6 +4,7 @@ import logging
 import torch
 import omegaconf
 import yaml
+import requests
 
 from .taming.models.vqgan import VQModel, GumbelVQ
 
@@ -23,48 +24,48 @@ VQGAN_CONFIG_DICT = {
 }
 
 
-def load_vqgan(model_name: str = "imagenet-16384",):
+def load_vqgan(model_name: str = "imagenet-16384", ):
     modeling_dir = os.path.dirname(os.path.abspath(__file__))
-        modeling_cache_dir = os.path.join(modeling_dir, ".modeling_cache")
-        os.makedirs(modeling_cache_dir, exist_ok=True)
+    modeling_cache_dir = os.path.join(modeling_dir, ".modeling_cache")
+    os.makedirs(modeling_cache_dir, exist_ok=True)
 
-        modeling_ckpt_path = os.path.join(modeling_cache_dir,
-                                          f'{model_name}.ckpt')
-        if not os.path.exists(modeling_ckpt_path):
-            modeling_ckpt_url = VQGAN_CKPT_DICT[model_name]
+    modeling_ckpt_path = os.path.join(modeling_cache_dir, f'{model_name}.ckpt')
+    if not os.path.exists(modeling_ckpt_path):
+        modeling_ckpt_url = VQGAN_CKPT_DICT[model_name]
 
-            logging.info(
-                f"Downloading pre-trained weights for VQ-GAN from {modeling_ckpt_url}"
-            )
-            results = requests.get(modeling_ckpt_url, allow_redirects=True)
-
-            with open(modeling_ckpt_path, "wb") as ckpt_file:
-                ckpt_file.write(results.content)
-
-        # TODO: update the url with our own config using the correct paths
-        modeling_config_path = os.path.join(modeling_cache_dir,
-                                            f'{model_name}.yaml')
-        if not os.path.exists(modeling_config_path):
-            modeling_config_url = VQGAN_CONFIG_DICT[model_name]
-
-            logging.info(
-                f"Downloading `{model_name}.yaml` from vipermu taming-transformers fork"
-            )
-            results = requests.get(modeling_config_url, allow_redirects=True)
-
-            with open(modeling_config_path, "wb") as yaml_file:
-                yaml_file.write(results.content)
-
-        vqgan_config = load_config(
-            config_path=modeling_config_path,
-            display=False,
+        logging.info(
+            f"Downloading pre-trained weights for VQ-GAN from {modeling_ckpt_url}"
         )
-        vqgan_model = load_vqgan(
-            vqgan_config,
-            ckpt_path=modeling_ckpt_path,
-        )
+        results = requests.get(modeling_ckpt_url, allow_redirects=True)
 
-        return vqgan_model
+        with open(modeling_ckpt_path, "wb") as ckpt_file:
+            ckpt_file.write(results.content)
+
+    # TODO: update the url with our own config using the correct paths
+    modeling_config_path = os.path.join(modeling_cache_dir,
+                                        f'{model_name}.yaml')
+    if not os.path.exists(modeling_config_path):
+        modeling_config_url = VQGAN_CONFIG_DICT[model_name]
+
+        logging.info(
+            f"Downloading `{model_name}.yaml` from vipermu taming-transformers fork"
+        )
+        results = requests.get(modeling_config_url, allow_redirects=True)
+
+        with open(modeling_config_path, "wb") as yaml_file:
+            yaml_file.write(results.content)
+
+    vqgan_config = load_config(
+        config_path=modeling_config_path,
+        display=False,
+    )
+    vqgan_model = load_vqgan(
+        vqgan_config,
+        ckpt_path=modeling_ckpt_path,
+    )
+
+    return vqgan_model
+
 
 def load_vqgan(
     config: omegaconf.dictconfig.DictConfig,
@@ -93,6 +94,7 @@ def load_vqgan(
         _missing, _unexpected = model.load_state_dict(state_dict, strict=False)
 
     return model.eval()
+
 
 def load_config(
     config_path: str,
