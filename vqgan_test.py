90179fd7e1d9f458624221f49afff443c4f7ce06
vipermu <vipermu97@gmail.com>
2022-03-14 01:30:17 -0400

    wip

--- vqgan_test.py
+++ vqgan_test.py
@@ -1,9 +1,52 @@
+from typing import *
+
+import torch
+import PIL
+import PIL.Image
+import numpy as np
+
 from taming.modeling_utils import load_model
+
+
+def generation(img: PIL.Image, ):
+    model_name = "sber"
+    device = "cuda"
+
+    vqgan_model = load_model(model_name=model_name, ).to(device)
+
+    img = img.convert('RGB')
+    img_tensor = torch.tensor(np.asarray(img))
+    img_tensor = img_tensor.permute(2, 0, 1)
+    img_tensor = (img_tensor / 255.) * 2 - 1
+
+    if len(img_tensor.shape) <= 3:
+        img_tensor = img_tensor[None, ::]
+
+    img_tensor = img_tensor.float().to(device)
+
+    img_tensor = torch.nn.functional.interpolate(
+        img_tensor,
+        (int((img_tensor.shape[2] / 16) * 16),
+         int((img_tensor.shape[3] / 16) * 16)),
+        mode="bilinear",
+    )
+
+    z, _, [_, _, _indices] = vqgan_model.encode(img_tensor)
+
+    z = z.to(device)
+
+    z = vqgan_model.post_quant_conv(z)
+    z = torch.cat(
+        [z, z],
+        axis=1,
+    )
+    img_rec = vqgan_model.decoder(z)
+    img_rec = (img_rec.clip(-1, 1) + 1) / 2
 
 
 def test():
     try:
-        model_name = "openimages-8192"
+        model_name = "sber"
         model = load_model(model_name=model_name, )
         print("OK 👌")
 
@@ -13,4 +56,8 @@ def test():
 
 
 if __name__ == "__main__":
-    test()
+    # test()
+
+    img = PIL.Image.open("./img.jpg")
+    img = img.resize((200, 125))
+    generation(img)
